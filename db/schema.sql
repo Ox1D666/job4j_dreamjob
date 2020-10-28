1311ae207a38010b8ad26bc4bc82900d237a0ccf
Kirill Romanov <kirill.coldline@gmail.com>
2020-10-28 16:23:42 +0300

    add sql scripts for create tables candidate & photo

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,13 @@ CREATE TABLE IF NOT EXISTS post (
    id SERIAL PRIMARY KEY,
    name TEXT
 );
+
+CREATE TABLE IF NOT EXISTS photo (
+   id SERIAL PRIMARY KEY,
+);
+
+CREATE TABLE IF NOT EXISTS candidate (
+   id SERIAL PRIMARY KEY,
+   name TEXT,
+   photoId int REFERENCES photo(id)
+);
